08d60b459cd784420ddd3d851465601b004af0b2
Shaun Molloy <shaunmolloy@gmail.com>
2026-05-10 10:10:00 +0100

    Replace js smooth scrolling with css

--- index.script.js
+++ index.script.js
@@ -70,15 +70,3 @@ window.addEventListener('keydown', (e) => {
 				closeModal();
     }
 });
-
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-		anchor.addEventListener('click', function(e) {
-				e.preventDefault();
-
-				document.querySelector(this.getAttribute('href'))
-						.scrollIntoView({
-								behavior: 'smooth'
-						});
-		});
-});
